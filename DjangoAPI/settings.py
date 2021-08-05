61878647338dfd9c373279a4dc59429a4db6606e
Oladipupo Ogunlana <dipson2solid@yahoo.com>
2021-08-05 20:39:22 +0100

    updated settings.py by removing Django Secret Key

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=0kk&80h)4tvk!qpk@tf4netsd82!m9lxdfzc6by4!1(6mz&z6'
+SECRET_KEY = '****************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
